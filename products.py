f677bd139c40a18cbaa5c4fc91dd0eb018e8f893
ramakrishnanooka1359-dotcom <ramakrishnanooka1359@gmail.com>
2026-02-09 18:59:10 +0530

    added a new line

--- products.py
+++ products.py
@@ -1,38 +1,5 @@
 from odoo_rpc import OdooRPC
 from config import ODOO_URL, ODOO_DB, ODOO_USERNAME, ODOO_API_KEY
-<<<<<<< HEAD
-
-
-def fetch_products():
-    """
-    Fetch product VARIANTS from Odoo using JSON-RPC
-    """
-    odoo = OdooRPC(
-        ODOO_URL,
-        ODOO_DB,
-        ODOO_USERNAME,
-        ODOO_API_KEY
-    )
-
-    products = odoo.call(
-        model="product.product",
-        method="search_read",
-        args=[[]],
-        kwargs={
-            "fields": [
-                "id",
-                "name",
-                "default_code",
-                "list_price",
-                "qty_available"
-            ],
-            "limit": 100
-        }
-    )
-
-    return products
-
-=======
 from product_creator import create_or_update_product_template
 
 
@@ -78,4 +45,3 @@ def create_all_products():
     create_or_update_product_template("Curd", "Kilogram")
     create_or_update_product_template("Ghee", "Kilogram")
     create_or_update_product_template("Paneer", "Kilogram")
->>>>>>> c33a002 (added a new line)
